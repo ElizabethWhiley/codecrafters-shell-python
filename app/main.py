92442a3a48f1874df4dddb1d442dd97b2f74ceb8
ElizabethWhiley <53459911+ElizabethWhiley@users.noreply.github.com>
2025-12-22 10:39:40 +1100

    Implement command prompt loop in main function for continuous input reading

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
     # read the input from the user and remove newline char
-    input = sys.stdin.readline().strip()
-    sys.stdout.write(input + ": command not found")
+    valid = False
+    while not valid:
+        sys.stdout.write("$ ")
+        input = sys.stdin.readline().strip()
+        sys.stdout.write(input + ": command not found")
     pass
 
 
