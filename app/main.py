c10224adbef33a10cd4f56da33db53d87d0bfab0
ElizabethWhiley <53459911+ElizabethWhiley@users.noreply.github.com>
2025-12-22 10:20:02 +1100

    Added input reading and error message for unrecognized commands in main function

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    # read the input from the user
+    input = sys.stdin.readline()
+    # print the input
+    sys.stdout.write(input + ": command not found")
     pass
 
 
