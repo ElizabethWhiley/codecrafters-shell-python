e5a089ce061a51528c24ced0f8c18afbe6718519
ElizabethWhiley <53459911+ElizabethWhiley@users.noreply.github.com>
2025-12-22 10:31:00 +1100

    Refactor input reading in main function to streamline newline removal

--- app/main.py
+++ app/main.py
@@ -4,11 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    # read the input from the user
-    input = sys.stdin.readline()
-
-    # and remove the newline character
-    input = input.strip()
+    # read the input from the user and remove newline char
+    input = sys.stdin.readline().strip()
     sys.stdout.write(input + ": command not found")
     pass
 
