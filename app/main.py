2d3e48c4172b5aa195c14d2b4f0dac5b9e5f2a32
ElizabethWhiley <53459911+ElizabethWhiley@users.noreply.github.com>
2025-12-22 11:32:39 +1100

    Introduce built-in command handling for 'echo' and 'exit' in main function

--- app/main.py
+++ app/main.py
@@ -2,16 +2,21 @@ import sys
 
 
 def main():
+    builtins = ["echo", "exit"]
     # read the input from the user and remove newline char
     while True:
         sys.stdout.write("$ ")
         input = sys.stdin.readline().strip()
-        if input == "exit":
-            sys.exit(0)
+        # if input == "exit":
+        #     sys.exit(0)
         command = input.split()[0]
         arguments = input.split()[1:]
-        if command == "echo":
-            sys.stdout.write(" ".join(arguments) + "\n")
+
+        if command in builtins:
+            sys.stdout.write(command + " is a shell builtin\n")
+
+        # if command == "echo":
+        #     sys.stdout.write(" ".join(arguments) + "\n")
         else:
             sys.stdout.write(command + ": command not found\n")
     pass
