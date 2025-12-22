9b2aba5a65f5b5b8ac841aa5de85d938187a6d76
ElizabethWhiley <53459911+ElizabethWhiley@users.noreply.github.com>
2025-12-22 11:50:30 +1100

    Enhance built-in command functionality by adding 'type' support and refactoring command execution in main function

--- app/main.py
+++ app/main.py
@@ -1,26 +1,35 @@
 import sys
 
 
+builtins = ["echo", "exit", "type"]
+
 def main():
-    builtins = ["echo", "exit"]
-    # read the input from the user and remove newline char
     while True:
         sys.stdout.write("$ ")
         input = sys.stdin.readline().strip()
-        # if input == "exit":
-        #     sys.exit(0)
         command = input.split()[0]
         arguments = input.split()[1:]
-
-        if command in builtins:
-            sys.stdout.write(command + " is a shell builtin\n")
-
-        # if command == "echo":
-        #     sys.stdout.write(" ".join(arguments) + "\n")
+        if is_builtin(command):
+            execute_builtin(command, arguments)
         else:
             sys.stdout.write(command + ": command not found\n")
-    pass
 
+def is_builtin(command):
+    return command in builtins
+
+def execute_builtin(command, arguments):
+    if command == "type":
+            for arg in arguments:
+                if arg in builtins:
+                    sys.stdout.write(arg + " is a shell builtin\n")
+                else:
+                    sys.stdout.write(arg + ": not found\n")
+
+    if command == "echo":
+        sys.stdout.write(" ".join(arguments) + "\n")
+
+    if command == "exit":
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
